add94c396dfa21dd3a165a9a9f6863da0338b235
adogbeji <baka_1993@yahoo.co.uk>
2024-02-20 11:34:15 +0000

    Start adding JavaScript & more SVG icons for home page (logged in) of Instagram Clone

---
+++ rails-courses/rails-course-1/projects/rails-7/instagram/logged-in-pages/home-page/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+// SEARCH BAR CLOSE ICON
+const closeIcon = document.querySelector('._0057-sect_1-topnav__search-clear-input');
+const input = document.querySelector('._0057-sect_1-topnav__search-input');
+
+// Removes close icon when nothing is typed
+input.addEventListener('input', function(e) {
+    if (input.value !== '') {
+        closeIcon.classList.remove('_0057-sect_1-topnav__search-clear-input--hide');
+    } else {
+        closeIcon.classList.add('_0057-sect_1-topnav__search-clear-input--hide');
+    }
+});
+
+
+// SEARCH INPUT DROPDOWN BOX 
+const inputDropdownBox = document.querySelector('._0057-sect_1-topnav__search-input-dropdown-box');
+const dropdownContent = document.querySelector('._0057-sect_1-topnav__search-dropdown-content');
+
+input.addEventListener('focus', function() {
+  inputDropdownBox.style.flex = '0 0 75%';
+  dropdownContent.style.minWidth = '52.3rem';
+});
+
+input.addEventListener('focusout', function() {
+  inputDropdownBox.style.flex = '0 0 70%';
+  dropdownContent.style.minWidth = '48.8rem';
+});
